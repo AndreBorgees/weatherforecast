5115f9f34f8298603bde4e8e62c46d636100d68b
Andre <borges.andree@gmail.com>
2022-12-28 19:56:57 -0300

    First commit

---
+++ weatherforecast/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace weatherforecast.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            HttpClient client = new HttpClient();
+            var url = "https://api.hgbrasil.com/weather?locale=pt";
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            var weather = response.Content.ReadAsStringAsync();
+
+            return weather.Result;
+        }
+    }
+}
