2885b2b8e1fac6ad5afe4dc117c87563721c5cef
André Borges <compassouol-andre.borges@ailos.coop.br>
2023-02-03 16:33:19 -0300

    testando deploy

--- weatherforecast/Controllers/WeatherForecastController.cs
+++ weatherforecast/Controllers/WeatherForecastController.cs
@@ -22,7 +22,7 @@ namespace weatherforecast.Controllers
             HttpResponseMessage response = client.GetAsync(url).Result;
             var weather = response.Content.ReadAsStringAsync();
 
-            return weather.Result + "ei";
+            return weather.Result + "eieee";
         }
     }
 }
